afeedbbcbfc7d449a0a4c5074e04b64b420183de
NiklasReents <Niklas.Reents@gmail.com>
2020-05-27 10:45:55 +0200

    create router structure in app.js and site_components folder (views: home, login, register)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./site_components/home.js";
+import Register from "./site_components/register.js";
+import Login from "./site_components/login.js";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div>
+          <Link className="link" to="/">
+            <button>Home</button>
+          </Link>
+          <span>|</span>
+          <Link className="link" to="/register">
+            <button>Register</button>
+          </Link>
+          <span>|</span>
+          <Link className="link" to="/login">
+            <button>Login</button>
+          </Link>
+        </div>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
