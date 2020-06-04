a356d6d830269e9f93bfa3da59dba6a7283ceed2
NiklasReents <Niklas.Reents@gmail.com>
2020-06-04 12:26:20 +0200

    add axios data fetch from db, create + set state variables for dynamic data display

--- src/site_components/characterselection.js
+++ src/site_components/characterselection.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styles from "../styles.css";
 import Cookies from "universal-cookie";
+import styles from "../styles.css";
 
 const cookies = new Cookies();
 
 export default function Characterselection() {
+  const [characters, setCharacters] = useState([]);
+  const [characterIndex, setCharacterIndex] = useState(0);
+
   useEffect(() => {
     axios
       .get("http://localhost:8000/login/characterselection", {
@@ -13,45 +16,44 @@ export default function Characterselection() {
       })
       .then(function (response) {
         console.log(response);
+        setCharacters(response.data);
       })
       .catch(function (error) {
         window.location.href = "/login";
       });
   }, []);
-
-  return (
-    <div className="characterselection">
-      <h3>Choose your Character!</h3>
-      <h4>Monopoly Man{/*to be fetched from db*/}</h4>
-      <h4>
-        A high risk-and-reward character whose money-based abilities are capable
-        of dealing very high damage to his opponent - or himself!
-        {/*to be fetched from db*/}
-      </h4>
-      <div className="imagecontainer">
-        <button>
-          <img
-            className="characterimage"
-            src="arrow_left.bmp"
-            alt="arrow_left"
-          />
-        </button>
-        <button>
-          <img
-            className="characterimage"
-            src="monopoly_man.bmp"
-            alt="monopoly_man"
-          />
-        </button>
-        {/*to be fetched from db*/}
-        <button>
-          <img
-            className="characterimage"
-            src="arrow_right.bmp"
-            alt="arrow_right"
-          />
-        </button>
+  if (characters.length) {
+    return (
+      <div className="characterselection">
+        <h3>Choose your Character!</h3>
+        <h4>{characters[characterIndex].name}</h4>
+        <h4>{characters[characterIndex].description}</h4>
+        <div className="imagecontainer">
+          <button>
+            <img
+              className="characterimage"
+              src="arrow_left.bmp"
+              alt="arrow_left"
+            />
+          </button>
+          <button>
+            <img
+              className="characterimage"
+              src={characters[characterIndex].image}
+              alt="monopoly_man"
+            />
+          </button>
+          <button>
+            <img
+              className="characterimage"
+              src="arrow_right.bmp"
+              alt="arrow_right"
+            />
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <div></div>;
+  }
 }
